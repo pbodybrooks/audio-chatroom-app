2ff2da62e4cf6375e94a42efd799aa450f416af7
Park <parkerlamarbrook@gmail.com>
2024-06-20 16:16:42 -0600

    sign in fields created

--- client/src/pages/sign-in/index.tsx
+++ client/src/pages/sign-in/index.tsx
@@ -1,3 +1,22 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 export const SignIn = () => {
-    return <div> Sign In </div>
-}
+  return (
+    <div className="sign-in">
+      {" "}
+      <h1> Welcome to Parker's Audio Chat Room </h1>{" "}
+      <form>
+        <div>
+            <label> Username: </label>
+            <input type="text" />
+        </div>
+        <div>
+            <label> Name: </label>
+            <input type="text" />
+        </div>
+        <button type="submit"> Sign In </button>
+      </form>
+    </div>
+  );
+};
